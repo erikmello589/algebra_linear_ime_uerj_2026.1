0c224df02655a49e51e70cf3d25e4018160d8181
Erik Mello <erikmello589@gmail.com>
2026-05-24 21:48:34 -0300

    Criando aumentada e pivoteamento

--- menu_vetor_matrizes.c
+++ menu_vetor_matrizes.c
@@ -3,60 +3,69 @@
 #include <locale.h>
 
 //declaração de constante
-#define ORDEM 5
+#define ORDEM 3
 #define FALSE -1
 #define TRUE 1
 
-/*1 - Soma de Vetores (Vetor1 + Vetor2 = vetor3);
-2 - Multiplicação Vetor x Escalar (Vetor1 * E = Vetor3);
-3 - Multiplicação Vetor x Vetor (Vetor1 * Vetor2 = Int);
-4 - Soma Matriz + Matriz (Matriz1 + Matriz2 = Matriz3);
-5 - Multiplicação Matriz x Escalar (Matriz * E = Matriz2);
-6 - Multiplicação Matriz x Matriz (Matriz1 * Matriz2 = Matriz3) */
-
 //protótipo das funções (para que a main fique por cima)
 void exibirVetor (int vetor[], int tamanhoVetor);
-void exibirMatriz (float matriz[ORDEM][1]);
+void exibirMatriz(int linhas, int colunas, float matriz[linhas][colunas]);
 
-void somaVetores(int vetor1[], int vetor2[], int ORDEMVetores); // vai retornar um vetor3 
-void multVEscalar(int vetor1[], float escalar, int ORDEMVetor); // vai retornar um vetor3
-void multVetor(int vetor1[], int vetor2[], int ORDEMVetores); // vai retornar um int
-void somaMatrizes(int matriz1[ORDEM][ORDEM], int matriz2[ORDEM][ORDEM], int ORDEMMatrizes); // vai retornar uma matriz3
-void multMEscalar(int matriz1[ORDEM][ORDEM], float escalar, int ORDEMMatriz); // vai retornar uma matriz3
+void somaVetores(int vetor1[], int vetor2[], int ORDEMVetores);
+void multVEscalar(int vetor1[], float escalar, int ORDEMVetor);
+void multVetor(int vetor1[], int vetor2[], int ORDEMVetores); 
+void somaMatrizes(int matriz1[ORDEM][ORDEM], int matriz2[ORDEM][ORDEM], int ORDEMMatrizes); 
+void multMEscalar(int matriz1[ORDEM][ORDEM], float escalar, int ORDEMMatriz); 
 
-
-void multMatriz(float matriz1[ORDEM][ORDEM], float matriz2[ORDEM][1], float matriz3[ORDEM][1]); // vai retornar uma matriz3
+void multMatriz(float matriz1[ORDEM][ORDEM], float matriz2[ORDEM][1], float matriz3[ORDEM][1]);
 
 void criar_matriz_solucao (float matriz_solucao[ORDEM][1]);
 void criar_matriz_hilbert (float matriz[ORDEM][ORDEM]);
+void criar_matriz_aumentada (float matriz1[ORDEM][ORDEM], float matriz2[ORDEM][1], float matriz3[ORDEM][ORDEM+1]);
+
 void eliminacao (int ordem, float matriz_hilbert[ORDEM][ORDEM], float matriz_solucao[ORDEM][1]);
 void substituicao_retroativa (int ordem, float matriz_hilbert[ORDEM][ORDEM], float matriz_solucao[ORDEM][1], float matriz_variavel[ORDEM][1]);
+void gauss_pivoteamento_parcial (int ordem, float matriz_aumentada[ORDEM][ORDEM+1], float matriz_variavel[ORDEM][1]);
 
 void main()
 {
 	setlocale(LC_ALL,"portuguese");
 	
-	float matriz_hilbert[ORDEM][ORDEM];
 	float matriz_solucao[ORDEM][1];
+	float matriz_hilbert[ORDEM][ORDEM];
+	float matriz_aumentada[ORDEM][ORDEM+1]; //Declarando a matriz aumentada (Â = A|b)
 	float matriz_variavel[ORDEM][1];
-	
 	float solucao_encontrada[ORDEM][1];
-	
-	//criando matriz hilbert
-	criar_matriz_hilbert(matriz_hilbert);
 	
 	//criando matriz solucao
 	criar_matriz_solucao(matriz_solucao);
 	
+	//criando matriz hilbert
+	criar_matriz_hilbert(matriz_hilbert);
+	
+	//criando matriz aumentada
+	criar_matriz_aumentada(matriz_hilbert, matriz_solucao, matriz_aumentada);
+		
+	/*
+	//Realizando a Eliminação de Gauss SEM o pivoteamento pacial.
 	eliminacao(ORDEM, matriz_hilbert, matriz_solucao);
-	
 	substituicao_retroativa(ORDEM, matriz_hilbert, matriz_solucao, matriz_variavel);
-	
 	exibirMatriz(matriz_variavel);
 	
+	//Testando a solução encontrada através da Eliminação de Gaus SEM o pivoteamento pacial.
 	criar_matriz_hilbert(matriz_hilbert);
 	multMatriz(matriz_hilbert, matriz_variavel, solucao_encontrada);
 	exibirMatriz(solucao_encontrada);
+	*/
+
+	//Realizando a Eliminação de Gauss COM o pivoteamento pacial.
+	gauss_pivoteamento_parcial(ORDEM, matriz_aumentada, matriz_variavel);
+	exibirMatriz(ORDEM, 1, matriz_variavel);
+	
+	//Testando a solução encontrada através da Eliminação de Gaus COM o pivoteamento pacial.
+	criar_matriz_hilbert(matriz_hilbert);
+	multMatriz(matriz_hilbert, matriz_variavel, solucao_encontrada);
+	exibirMatriz(ORDEM, 1, solucao_encontrada);
 
 }
 
@@ -168,25 +177,45 @@ void criar_matriz_solucao (float matriz_solucao[ORDEM][1])
 	}
 }
 
-void exibirMatriz (float matriz[ORDEM][1])
+void criar_matriz_aumentada (float matriz1[ORDEM][ORDEM], float matriz2[ORDEM][1], float matriz3[ORDEM][ORDEM+1])
 {
 	int i, j;
 	
-	printf ("\nElementos da matriz:\n\n");
-	
-	//varrendo as linhas da matriz
-	for (i=0;i<ORDEM;i++)
+	//percorrendo hilbert
+	for (i=0; i<ORDEM; i++)
 	{
-		//varrendo as colunas
-		for (j=0;j<1;j++)
+		for(j=0; j<ORDEM; j++)
 		{
-			printf ("%.2f ", matriz[i][j]);
+			matriz3[i][j] = matriz1[i][j];
 		}
-		
-		printf ("\n");
 	}
 	
-	printf ("\n\n");
+	//percorrido toda hilbert, então, agora colocar a solucao
+	for (i=0; i<ORDEM; i++)
+	{
+		matriz3[i][j] = matriz2[i][0];
+	}
+}
+
+void exibirMatriz(int linhas, int colunas, float matriz[linhas][colunas])
+{
+	int i, j;
+	
+	printf("\nElementos da matriz:\n\n");
+	
+	// varrendo as linhas da matriz
+	for (i = 0; i < linhas; i++)
+	{
+		// varrendo as colunas da matriz
+		for (j = 0; j < colunas; j++)
+		{
+			printf("%.10f ", matriz[i][j]);
+		}
+		
+		printf("\n");
+	}
+	
+	printf("\n\n");
 }
 
 void eliminacao (int ordem, float matriz_hilbert[ORDEM][ORDEM], float matriz_solucao[ORDEM][1])
@@ -229,4 +258,62 @@ void substituicao_retroativa (int ordem, float matriz_hilbert[ORDEM][ORDEM], flo
 		
 		matriz_variavel[i][0] = (matriz_solucao[i][0] - soma) / matriz_hilbert[i][i];
 	}
+}
+
+void gauss_pivoteamento_parcial (int ordem, float matriz_aumentada[ORDEM][ORDEM+1], float matriz_variavel[ORDEM][1])
+{
+	int i, j, k;
+	int posicao_maior_elemento;
+	float valor_maior_elemento;
+	float aux;
+	float matriz_multiplicadores[ORDEM][ORDEM+1];
+	float soma;
+	
+	for (k=0;k<ordem;k++)
+	{
+		valor_maior_elemento = matriz_aumentada[k][k];
+		posicao_maior_elemento = k; 
+		
+		for (i=k; i<ordem; i++)
+		{
+			if (matriz_aumentada[i][k] > valor_maior_elemento)
+			{
+				valor_maior_elemento = matriz_aumentada[i][k];
+				posicao_maior_elemento = i; 
+			}
+		}
+		
+		for (j=k; j<=ordem; j++)
+		{
+			aux = matriz_aumentada[k][j];
+			matriz_aumentada[k][j] = matriz_aumentada[posicao_maior_elemento][j];
+			matriz_aumentada[posicao_maior_elemento][j] = aux;
+		}
+		
+		for (i=k+1; i<ordem; i++)
+		{
+			matriz_multiplicadores[i][k] = matriz_aumentada[i][k] / matriz_aumentada[k][k];
+			
+			for (j=k+1; j<=ordem; j++)
+			{
+				matriz_aumentada[i][j] = matriz_aumentada[i][j] - (matriz_multiplicadores[i][k] * matriz_aumentada[k][j]);
+			}
+		}
+	}
+	
+	
+	ordem = ordem - 1;
+	matriz_variavel[ordem][0] = matriz_aumentada[ordem][ordem+1] / matriz_aumentada[ordem][ordem];
+	
+	for (i=ordem-1; i>=0; i--)
+	{
+		soma = 0;
+		
+		for (j=i+1; j<=ordem; j++)
+		{
+			soma = soma + (matriz_aumentada[i][j] * matriz_variavel[j][0]);
+		}
+		
+		matriz_variavel[i][0] = (matriz_aumentada[i][ordem+1] - soma) / matriz_aumentada[i][i];
+	}	
 }
